ad3ffa9e97a9610e190ec0a2414c15d509f76ec5
Motlatso <chabamotlatso@gmail.com>
2024-10-25 01:55:20 +0200

    Called all the functions from transactionListPage into Marker test3

--- Tests/test_tests.py
+++ Tests/test_tests.py
@@ -1,9 +1,12 @@
+from datetime import datetime, timedelta
+
 import allure
 import pytest
 
 from Pages.accountPage import AccountPage
 from Pages.customerPage import CustomerPage
 from Pages.loginPage import LoginPage
+from Pages.transactionListPage import TransactionListPage
 from Utils.readProperties import ReadConfigs
 
 
@@ -53,9 +56,10 @@ class Tests:
         self.customerLogin.selectUserLogin()
         self.customerLogin.clickLoginButton()
 
-        amountToDepositValue = 1500
+        # initialize amount to deposit
+        amountToDeposit = 1500
 
-        self.account.selectAllAccount(amountToDepositValue)
+        self.account.selectAllAccount(amountToDeposit)
 
         self.account.clickLogoutButton()
 
@@ -71,11 +75,33 @@ class Tests:
         self.login = LoginPage(self.driver)
         self.customerLogin = CustomerPage(self.driver)
         self.account = AccountPage(self.driver)
+        self.transactions = TransactionListPage(self.driver)
 
         self.login.clickCustomerLoginButton()
         self.customerLogin.selectUserLogin()
         self.customerLogin.clickLoginButton()
 
+        # initialize amount to deposit
+        amountToDeposit = 31459
+
+        self.account.selectFirstAccount()
+        balanceBeforeDepositValue = self.account.getBalanceBeforeTheDeposit()
+        self.account.clickDepositTabButton()
+        self.account.enterAmountToDeposit(amountToDeposit)
+        self.account.clickDepositSubmitButtion()
+        balanceAfterDepositValue = self.account.getBalanceAfterTheDeposit()
+        self.account.verifyIfDepositWasSuccessful(balanceBeforeDepositValue, balanceAfterDepositValue, amountToDeposit)
+        self.account.clickTransactionsTabButton()
+
+        # Get the current date
+        current_date = datetime.now()
+        start_datetime = current_date.strftime('%Y-%m-%d')
+
+        self.transactions.sendStartDate(start_datetime)
+
+        self.transactions.verifyTransactionIsInTheList(amountToDeposit)
+
+        self.account.clickLogoutButton()
 
 
 
